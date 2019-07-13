a85d84213fa504504ea7a3b254478994ba09b5ff
Ilyes Abd-Lillah <ilyes.abd-lillah@epitech.eu>
2019-07-13 14:10:19 +0200

    [0.0.1b5] Storybook, babel lib, config, lib file structure ready

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,7 +5,7 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Welcome } from '@storybook/react/demo';
-import Button from '../index';
+import { Button } from '../index';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
